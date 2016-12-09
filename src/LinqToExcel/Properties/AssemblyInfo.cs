d7cf1591a2abfa124626d7227450d4c54bd65637
UongHung <hungudgm@gmail.com>
2016-12-09 17:33:21 +0700

    UPT MOVE TO .NET 4.5.2

--- src/LinqToExcel/Properties/AssemblyInfo.cs
+++ src/LinqToExcel/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Yoder Web Solutions, LLC")]
 [assembly: AssemblyProduct("LinqToExcel")]
-[assembly: AssemblyCopyright("Copyright © Paul Yoder 2013")]
+[assembly: AssemblyCopyright("Copyright © Paul Yoder 2013 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.10.1")]
-[assembly: AssemblyFileVersion("1.10.1")]
+[assembly: AssemblyVersion("1.10.1.1")]
+[assembly: AssemblyFileVersion("1.10.1.1")]
 
 [assembly: System.Security.AllowPartiallyTrustedCallers]
